bff632aa53af975513b96250eb98e5fed0b803cb
VIPIN <VIPIN@192.168.1.43>
2022-09-24 14:39:31 +0530

    commit

---
+++ Tiwari/src/main/java/com/example/entity/Restaurant.java
@@ -0,0 +1,40 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurant {
+	@Id
+	private int restId;
+	private String restName;
+	private String restAddress;
+	private int restContact;
+	//private Food restMenu;
+	
+	public int getRestId() {
+		return restId;
+	}
+	public void setRestId(int restId) {
+		this.restId = restId;
+	}
+	public String getRestName() {
+		return restName;
+	}
+	public void setRestName(String restName) {
+		this.restName = restName;
+	}
+	public String getRestAddress() {
+		return restAddress;
+	}
+	public void setRestAddress(String restAddress) {
+		this.restAddress = restAddress;
+	}
+	public int getRestContact() {
+		return restContact;
+	}
+	public void setRestContact(int restContact) {
+		this.restContact = restContact;
+	}
+
+}
---
+++ Tiwari/src/test/java/com/example/Tiwari/TiwariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tiwari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiwariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
